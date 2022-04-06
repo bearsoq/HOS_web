f200537e5dec1ee76a3531e0c62ff4337d273c6c
bearsoq <71425673+bearsoq@users.noreply.github.com>
2022-04-06 12:18:02 +0800

    Delete 網頁 directory

--- 網頁/js/嚇拉.js
+++
@@ -1,7 +0,0 @@
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<script type="text/javascript">
-    $ (function (){
-        $("navigation ul > 1i:has (ul) > a").append('<div class="arrow-bottom"></div>');
-        $("navigation ul > li ul li:has (ul) > a").append('<div class="arrow-right">/div');
-    });
-</script>
